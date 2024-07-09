fea376156af87ceb8d95e20735fca1ee881660b8
odo27 <odo27@naver.com>
2024-07-10 00:06:24 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/gyeryongbrother/pickandtest/PickandtestApplication.java
@@ -0,0 +1,12 @@
+package com.gyeryongbrother.pickandtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickandtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickandtestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gyeryongbrother/pickandtest/PickandtestApplicationTests.java
@@ -0,0 +1,12 @@
+package com.gyeryongbrother.pickandtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickandtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
