18abdbabcc0e36f59e29a5818dddeb586cc6705e
odo27 <odo27@naver.com>
2024-07-10 13:28:29 +0900

    feat: 시가총액 엔티티 추가

---
+++ src/main/java/com/gyeryongbrother/pickandtest/dataaccess/entity/MarketCapitalizationEntity.java
@@ -0,0 +1,28 @@
+package com.gyeryongbrother.pickandtest.dataaccess.entity;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
+public class MarketCapitalizationEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private StockEntity stock;
+
+    private LocalDate date;
+    private Long value;
+}
