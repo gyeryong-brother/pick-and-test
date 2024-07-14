6d878dc3485a696e49a3ff770728e6d2720d3e93
odo27 <odo27@naver.com>
2024-07-14 20:11:02 +0900

    refactor: 주식 매핑 로직 클라이언트에서 분리

--- src/main/java/com/gyeryongbrother/pickandtest/infrastructure/adapter/StockProviderImpl.java
+++ src/main/java/com/gyeryongbrother/pickandtest/infrastructure/adapter/StockProviderImpl.java
@@ -3,6 +3,10 @@ package com.gyeryongbrother.pickandtest.infrastructure.adapter;
 import com.gyeryongbrother.pickandtest.domain.core.Stock;
 import com.gyeryongbrother.pickandtest.domain.service.ports.output.StockProvider;
 import com.gyeryongbrother.pickandtest.infrastructure.client.koreainvestment.KoreaInvestmentClient;
+import com.gyeryongbrother.pickandtest.infrastructure.client.koreainvestment.stock.StockExchange;
+import com.gyeryongbrother.pickandtest.infrastructure.client.koreainvestment.stock.dto.FetchStockResponse;
+import com.gyeryongbrother.pickandtest.infrastructure.client.koreainvestment.stock.dto.StockInformation;
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +18,19 @@ public class StockProviderImpl implements StockProvider {
 
     @Override
     public Stock provide(String symbol) {
-        return koreaInvestmentClient.fetchStock(symbol);
+        FetchStockResponse fetchStockResponse = koreaInvestmentClient.fetchStock(StockExchange.NASDAQ, symbol);
+        StockInformation stockInformation = fetchStockResponse.stockInformation();
+        return Stock.builder()
+                .name(stockInformation.productEnglishName())
+                .symbol(symbol)
+                .listingDate(parse(stockInformation.listingDate()))
+                .build();
+    }
+
+    private LocalDate parse(String listingDate) {
+        if (listingDate.isBlank()) {
+            return null;
+        }
+        return LocalDate.parse(listingDate);
     }
 }
