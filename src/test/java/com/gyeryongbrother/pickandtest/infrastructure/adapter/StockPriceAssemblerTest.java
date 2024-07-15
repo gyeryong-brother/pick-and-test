42ee7c73fc847c6cc20f1288f8bd9cb00b7a2748
odo27 <odo27@naver.com>
2024-07-15 16:45:53 +0900

    refactor: AssembleStockPriceResult -> StockPriceAssembler 로 변경

--- src/test/java/com/gyeryongbrother/pickandtest/infrastructure/adapter/StockPriceAssemblerTest.java
+++ src/test/java/com/gyeryongbrother/pickandtest/infrastructure/adapter/StockPriceAssemblerTest.java
@@ -1,59 +1,116 @@
 package com.gyeryongbrother.pickandtest.infrastructure.adapter;
 
 import static com.gyeryongbrother.pickandtest.infrastructure.client.koreainvestment.stockprice.dto.FetchStockPriceResponseFixture.firstFetchStockPriceResponse;
-import static com.gyeryongbrother.pickandtest.infrastructure.client.koreainvestment.stockprice.dto.FetchStockPriceResponseFixture.secondFetchStockPriceResponse;
-import static com.gyeryongbrother.pickandtest.infrastructure.client.koreainvestment.stockprice.dto.FetchStockPriceResponseFixture.thirdFetchStockPriceResponse;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.BDDMockito.given;
 
 import com.gyeryongbrother.pickandtest.domain.core.StockPrice;
-import com.gyeryongbrother.pickandtest.infrastructure.client.koreainvestment.KoreaInvestmentClient;
+import com.gyeryongbrother.pickandtest.infrastructure.client.koreainvestment.stockprice.ContinuityCode;
+import com.gyeryongbrother.pickandtest.infrastructure.client.koreainvestment.stockprice.dto.FetchStockPriceResponse;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 import org.assertj.core.util.BigDecimalComparator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
 class StockPriceAssemblerTest {
-
-    @Mock
-    private KoreaInvestmentClient koreaInvestmentClient;
 
     private StockPriceAssembler stockPriceAssembler;
 
     @BeforeEach
     void setUp() {
-        stockPriceAssembler = new StockPriceAssembler(koreaInvestmentClient);
+        stockPriceAssembler = new StockPriceAssembler();
+    }
+
+    @Test
+    void hasNextTrueWithEmpty() {
+        // when
+        boolean result = stockPriceAssembler.hasNext();
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void hasNextTrueWithNext() {
+        // given
+        FetchStockPriceResponse fetchStockPriceResponse = new FetchStockPriceResponse(ContinuityCode.NEXT, null);
+        stockPriceAssembler.add(fetchStockPriceResponse);
+
+        // when
+        boolean result = stockPriceAssembler.hasNext();
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void hasNextFalseWithEnd() {
+        // given
+        FetchStockPriceResponse nextResponse = new FetchStockPriceResponse(ContinuityCode.NEXT, null);
+        stockPriceAssembler.add(nextResponse);
+        FetchStockPriceResponse endResponse = new FetchStockPriceResponse(ContinuityCode.END, null);
+        stockPriceAssembler.add(endResponse);
+
+        // when
+        boolean result = stockPriceAssembler.hasNext();
+
+        // then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void getNextDateWithEmpty() {
+        // given
+        LocalDate expected = LocalDate.now();
+
+        // when
+        LocalDate result = stockPriceAssembler.getNextDate();
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void getNextDate() {
+        // given
+        FetchStockPriceResponse fetchStockPriceResponse = firstFetchStockPriceResponse();
+        stockPriceAssembler.add(fetchStockPriceResponse);
+        LocalDate expected = LocalDate.of(2024, 7, 9);
+
+        // when
+        LocalDate result = stockPriceAssembler.getNextDate();
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void getStockPricesWithEmpty() {
+        // given
+        List<StockPrice> expected = List.of();
+
+        // when
+        List<StockPrice> result = stockPriceAssembler.getStockPrices();
+
+        // then
+        assertThat(result).usingRecursiveComparison()
+                .isEqualTo(expected);
     }
 
     @Test
     void getStockPrices() {
         // given
-        given(koreaInvestmentClient.fetchStockPrice(any(), anyString(), any())).willReturn(
-                firstFetchStockPriceResponse(),
-                secondFetchStockPriceResponse(),
-                thirdFetchStockPriceResponse()
-        );
+        FetchStockPriceResponse fetchStockPriceResponse = firstFetchStockPriceResponse();
+        stockPriceAssembler.add(fetchStockPriceResponse);
         List<StockPrice> expected = List.of(
                 stockPrice(12, 230.54),
                 stockPrice(11, 227.57),
-                stockPrice(10, 232.98),
-                stockPrice(9, 228.68),
-                stockPrice(8, 227.82),
-                stockPrice(5, 226.34),
-                stockPrice(3, 221.55),
-                stockPrice(2, 220.27)
+                stockPrice(10, 232.98)
         );
 
         // when
-        List<StockPrice> result = stockPriceAssembler.getStockPrices("AAPL");
+        List<StockPrice> result = stockPriceAssembler.getStockPrices();
 
         // then
         assertThat(result).usingRecursiveComparison()
