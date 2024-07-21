70548d49e189d2752ab45440e210ec83aa8b20cd
odo27 <odo27@naver.com>
2024-07-21 16:43:27 +0900

    feat: 주식아이디로 주가 다건 조회 구현

---
+++ src/main/java/com/gyeryongbrother/pickandtest/domain/service/ports/output/StockPriceQueryRepository.java
@@ -0,0 +1,9 @@
+package com.gyeryongbrother.pickandtest.domain.service.ports.output;
+
+import com.gyeryongbrother.pickandtest.domain.service.dto.StockPriceResponse;
+import java.util.List;
+
+public interface StockPriceQueryRepository {
+
+    List<StockPriceResponse> findAllByStockId(Long stockId);
+}
