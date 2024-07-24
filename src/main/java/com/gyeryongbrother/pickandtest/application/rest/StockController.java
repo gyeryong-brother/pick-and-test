cf82b1bd01795f4a2404ce087fd76f15a9a57cd1
odo27 <odo27@naver.com>
2024-07-25 00:51:26 +0900

    feat: 관심 주식 추가 api 구현

--- src/main/java/com/gyeryongbrother/pickandtest/application/rest/StockController.java
+++ src/main/java/com/gyeryongbrother/pickandtest/application/rest/StockController.java
@@ -1,11 +1,20 @@
 package com.gyeryongbrother.pickandtest.application.rest;
 
+import static org.springframework.http.HttpStatus.CREATED;
+
+import com.gyeryongbrother.pickandtest.application.dto.CreateFavoriteStockRequest;
+import com.gyeryongbrother.pickandtest.domain.service.dto.CreateFavoriteStockCommand;
+import com.gyeryongbrother.pickandtest.domain.service.dto.CreateFavoriteStockResponse;
 import com.gyeryongbrother.pickandtest.domain.service.dto.StockResponse;
 import com.gyeryongbrother.pickandtest.domain.service.ports.input.StockQueryService;
+import com.gyeryongbrother.pickandtest.domain.service.ports.input.StockService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +25,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class StockController {
 
     private final StockQueryService stockQueryService;
+    private final StockService stockService;
 
     @GetMapping
     ResponseEntity<List<StockResponse>> searchStocks(@RequestParam String keyword) {
         List<StockResponse> stockResponses = stockQueryService.findAllByNameOrSymbol(keyword);
         return ResponseEntity.ok(stockResponses);
+    }
+
+    @PostMapping("/{stockId}/favorite")
+    ResponseEntity<CreateFavoriteStockResponse> createFavoriteStock(
+            @PathVariable Long stockId,
+            @RequestBody CreateFavoriteStockRequest createFavoriteStockRequest
+    ) {
+        CreateFavoriteStockCommand createFavoriteStockCommand = createFavoriteStockRequest.toCommand(stockId);
+        CreateFavoriteStockResponse createFavoriteStockResponse =
+                stockService.createFavoriteStock(createFavoriteStockCommand);
+        return ResponseEntity.status(CREATED)
+                .body(createFavoriteStockResponse);
     }
 }
--- src/test/java/com/gyeryongbrother/pickandtest/application/rest/StockControllerTest.java
+++ src/test/java/com/gyeryongbrother/pickandtest/application/rest/StockControllerTest.java
@@ -3,12 +3,16 @@ package com.gyeryongbrother.pickandtest.application.rest;
 import static com.gyeryongbrother.pickandtest.dataaccess.entity.StockEntityFixture.stockEntity;
 import static com.gyeryongbrother.pickandtest.domain.service.dto.StockResponseFixture.stockResponse;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+import com.gyeryongbrother.pickandtest.application.dto.CreateFavoriteStockRequest;
 import com.gyeryongbrother.pickandtest.dataaccess.repository.StockJpaRepository;
+import com.gyeryongbrother.pickandtest.domain.service.dto.CreateFavoriteStockResponse;
 import com.gyeryongbrother.pickandtest.domain.service.dto.StockResponse;
 import io.restassured.RestAssured;
 import io.restassured.common.mapper.TypeRef;
+import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import java.util.List;
@@ -64,5 +68,32 @@ class StockControllerTest {
         assertThat(result).usingRecursiveComparison()
                 .ignoringExpectedNullFields()
                 .isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("관심 주식을 저장한다")
+    void createFavoriteStock() {
+        // given
+        CreateFavoriteStockRequest createFavoriteStockRequest = new CreateFavoriteStockRequest(1L);
+        Long stockId = stockJpaRepository.save(stockEntity())
+                .getId();
+        CreateFavoriteStockResponse expected = new CreateFavoriteStockResponse(null, 1L, stockId);
+
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(createFavoriteStockRequest)
+                .when().post("/stocks/{stockId}/favorite", stockId)
+                .then().log().all()
+                .extract();
+        CreateFavoriteStockResponse result = response.as(CreateFavoriteStockResponse.class);
+
+        // then
+        assertAll(
+                () -> assertThat(result.id()).isPositive(),
+                () -> assertThat(result).usingRecursiveComparison()
+                        .ignoringExpectedNullFields()
+                        .isEqualTo(expected)
+        );
     }
 }
