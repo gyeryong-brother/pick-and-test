20ce21c9a4a92a8db3843f099eed1b5334668b0a
odo27 <odo27@naver.com>
2024-07-25 17:31:50 +0900

    feat: 주식과 주가를 가진 도메인 객체 추가

--- src/main/java/com/gyeryongbrother/pickandtest/domain/core/StockWithPrices.java
+++ src/main/java/com/gyeryongbrother/pickandtest/domain/core/StockWithPrices.java
@@ -2,15 +2,18 @@ package com.gyeryongbrother.pickandtest.domain.core;
 
 import java.util.List;
 import lombok.Builder;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@Getter
 @Builder
 @RequiredArgsConstructor
-public class StockDetail {
+public class StockWithPrices {
 
     private final Stock stock;
     private final List<StockPrice> stockPrices;
-    private final List<Dividend> dividends;
+
+    public List<MarketCapitalization> getMarketCapitalizations() {
+        return stockPrices.stream()
+                .map(stock::calculateMarketCapitalization)
+                .toList();
+    }
 }
--- src/test/java/com/gyeryongbrother/pickandtest/domain/core/StockWithPricesTest.java
+++ src/test/java/com/gyeryongbrother/pickandtest/domain/core/StockWithPricesTest.java
@@ -1,26 +1,26 @@
 package com.gyeryongbrother.pickandtest.domain.core;
 
 import static com.gyeryongbrother.pickandtest.domain.core.MarketCapitalizationFixture.marketCapitalizations;
-import static com.gyeryongbrother.pickandtest.domain.core.StockDetailFixture.stockDetail;
 import static com.gyeryongbrother.pickandtest.domain.core.StockPriceFixture.stockPrices;
+import static com.gyeryongbrother.pickandtest.domain.core.StockWithPricesFixture.stockWithPrices;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("주식 상세와 관련된 기능을 제공한다")
-class StockDetailTest {
+@DisplayName("주가들과 관련된 기능을 제공한다")
+class StockWithPricesTest {
 
     @Test
     @DisplayName("시가총액을 가져온다")
     void getMarketCapitalizations() {
         // given
-        StockDetail stockDetail = stockDetail(1L, stockPrices(1L), List.of());
+        StockWithPrices stockWithPrices = stockWithPrices(1L, stockPrices(1L));
         List<MarketCapitalization> expected = marketCapitalizations();
 
         // when
-        List<MarketCapitalization> result = stockDetail.getMarketCapitalizations();
+        List<MarketCapitalization> result = stockWithPrices.getMarketCapitalizations();
 
         // then
         assertThat(result).usingRecursiveComparison()
