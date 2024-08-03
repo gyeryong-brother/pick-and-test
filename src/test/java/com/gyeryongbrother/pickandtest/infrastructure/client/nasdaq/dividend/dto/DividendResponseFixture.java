690d7699c6f9fb6ae99f928ca2d15ee0cc20799a
odo27 <odo27@naver.com>
2024-08-03 22:53:28 +0900

    fix: api 사용 오류 해결

--- src/test/java/com/gyeryongbrother/pickandtest/infrastructure/client/nasdaq/dividend/dto/DividendResponseFixture.java
+++ src/test/java/com/gyeryongbrother/pickandtest/infrastructure/client/nasdaq/dividend/dto/DividendResponseFixture.java
@@ -6,25 +6,25 @@ public class DividendResponseFixture {
 
     public static DividendResponse appleDividendResponse() {
         return new DividendResponse(new DividendData(new Dividends(List.of(
-                new DividendDetail("2023-08-10", "0.24"),
-                new DividendDetail("2023-05-10", "0.24"),
-                new DividendDetail("2023-02-10", "0.23")
+                new DividendDetail("08/10/2023", "$0.24"),
+                new DividendDetail("05/10/2023", "$0.24"),
+                new DividendDetail("02/10/2023", "$0.23")
         ))));
     }
 
     public static DividendResponse microsoftDividendResponse() {
         return new DividendResponse(new DividendData(new Dividends(List.of(
-                new DividendDetail("2023-08-10", "0.68"),
-                new DividendDetail("2023-05-10", "0.68"),
-                new DividendDetail("2023-02-10", "0.68")
+                new DividendDetail("08/10/2023", "$0.68"),
+                new DividendDetail("05/10/2023", "$0.68"),
+                new DividendDetail("02/10/2023", "$0.68")
         ))));
     }
 
     public static DividendResponse nvidiaDividendResponse() {
         return new DividendResponse(new DividendData(new Dividends(List.of(
-                new DividendDetail("2023-09-10", "0.04"),
-                new DividendDetail("2023-06-10", "0.04"),
-                new DividendDetail("2023-03-10", "0.04")
+                new DividendDetail("09/10/2023", "$0.04"),
+                new DividendDetail("06/10/2023", "$0.04"),
+                new DividendDetail("03/10/2023", "$0.04")
         ))));
     }
 }
