cad5a01462f25644ce760bbf8805b1b1d1d25927
jusikjoa <brian20037463@gmail.com>
2025-01-14 18:57:51 +0900

    feat: PortfolioStockJpaRepository에 deleteAllByPortfolioEntity_Id 추가

--- portfolio-service/portfolio-dataaccess/src/main/java/com/gyeryongbrother/pickandtest/portfolio/dataaccess/repository/PortfolioStockJpaRepository.java
+++ portfolio-service/portfolio-dataaccess/src/main/java/com/gyeryongbrother/pickandtest/portfolio/dataaccess/repository/PortfolioStockJpaRepository.java
@@ -1,7 +1,11 @@
 package com.gyeryongbrother.pickandtest.portfolio.dataaccess.repository;
 
 import com.gyeryongbrother.pickandtest.portfolio.dataaccess.entity.PortfolioStockEntity;
+import com.gyeryongbrother.pickandtest.portfolio.domain.core.entity.PortfolioStock;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PortfolioStockJpaRepository extends JpaRepository<PortfolioStockEntity, Long> {
+
+    void deleteAllByPortfolioEntity_Id(Long portfolioId);
 }
