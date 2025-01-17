8557467fa428807729aae45daae38da3e4d70afe
jusikjoa <brian20037463@gmail.com>
2025-01-17 19:22:04 +0900

    feat: PortfolioFixture, PortfolioStockFixture 추가

---
+++ portfolio-service/portfolio-domain/portfolio-domain-core/src/test/java/com/gyeryongbrother/pickandtest/portfolio/domain/core/entity/PortfolioFixture.java
@@ -0,0 +1,35 @@
+package com.gyeryongbrother.pickandtest.portfolio.domain.core.entity;
+
+import java.util.List;
+
+public class PortfolioFixture {
+
+    //포트폴리오1=애플 50% + 마이크로소프트 50% ->멤버 1의 포트폴리오
+    //포트폴리오2=엔비디아 50% + 테슬라 50% ->멤버2의 포트폴리오
+    //포트폴리오3=구글 40% +메타 30% + 아마존 30%->멤버 1의 포트폴리오
+
+    public static Portfolio portfolio1(){
+        List<PortfolioStock> portfolioStocks=List.of(
+                PortfolioStockFixture.apple(),
+                PortfolioStockFixture.microsoft()
+        );
+        return new Portfolio(null,1L,portfolioStocks);
+    }
+
+    public static Portfolio portfolio2(){
+        List<PortfolioStock> portfolioStocks=List.of(
+                PortfolioStockFixture.nvidia(),
+                PortfolioStockFixture.tesla()
+        );
+        return new Portfolio(null,2L,portfolioStocks);
+    }
+
+    public static Portfolio portfolio3(){
+        List<PortfolioStock> portfolioStocks=List.of(
+                PortfolioStockFixture.google(),
+                PortfolioStockFixture.meta(),
+                PortfolioStockFixture.amazon()
+        );
+        return new Portfolio(null,1L,portfolioStocks);
+    }
+}
