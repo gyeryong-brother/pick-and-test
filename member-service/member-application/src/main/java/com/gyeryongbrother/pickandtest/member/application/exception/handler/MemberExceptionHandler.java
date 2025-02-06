10c162baeb668f40b2986fe767bd14d77e8e33e1
odo27 <odo27@naver.com>
2025-02-06 09:36:23 +0900

    feat: MemberException 추가

---
+++ member-service/member-application/src/main/java/com/gyeryongbrother/pickandtest/member/application/exception/handler/MemberExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.gyeryongbrother.pickandtest.member.application.exception.handler;
+
+import static com.gyeryongbrother.pickandtest.member.application.exception.handler.dto.ErrorResponse.INTERNAL_SERVER_ERROR_RESPONSE;
+
+import com.gyeryongbrother.pickandtest.member.application.exception.handler.dto.ErrorResponse;
+import com.gyeryongbrother.pickandtest.member.domain.service.exception.BaseException;
+import com.gyeryongbrother.pickandtest.member.domain.service.exception.BaseExceptionType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class MemberExceptionHandler {
+
+    @ExceptionHandler
+    ResponseEntity<ErrorResponse> handleException(BaseException exception) {
+        BaseExceptionType exceptionType = exception.exceptionType();
+        return ResponseEntity.status(exceptionType.httpStatus())
+                .body(new ErrorResponse(exceptionType.errorMessage()));
+    }
+
+    @ExceptionHandler
+    ResponseEntity<ErrorResponse> handleException(Exception exception) {
+        return ResponseEntity.internalServerError()
+                .body(INTERNAL_SERVER_ERROR_RESPONSE);
+    }
+}
