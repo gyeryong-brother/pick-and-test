3cc8392e96e502148c9b45bffb87d51e2d4a8778
jusikjoa <brian20037463@gmail.com>
2025-03-09 12:32:37 +0900

    feat: JwtAuthenticationFilter 구현

--- member-service/member-domain/member-application-service/src/main/java/com/gyeryongbrother/pickandtest/member/domain/service/ports/output/JwtUtil.java
+++ member-service/member-domain/member-application-service/src/main/java/com/gyeryongbrother/pickandtest/member/domain/service/ports/output/JwtUtil.java
@@ -2,6 +2,7 @@ package com.gyeryongbrother.pickandtest.member.domain.service.ports.output;
 
 import com.gyeryongbrother.pickandtest.member.domain.core.UserRole;
 import io.jsonwebtoken.Claims;
+import org.springframework.security.core.Authentication;
 
 public interface JwtUtil {
 
@@ -16,4 +17,6 @@ public interface JwtUtil {
     Long getMemberIdFromToken(String token);
 
     UserRole getRoleFromToken(String token);
+
+    Authentication getAuthentication(String token);
 }
