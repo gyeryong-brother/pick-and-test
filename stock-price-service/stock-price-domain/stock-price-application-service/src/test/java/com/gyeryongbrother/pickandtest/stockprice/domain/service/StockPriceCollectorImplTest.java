97e86b0295230abf0ec5067ae9d1605507edcd96
odo27 <odo27@naver.com>
2025-03-28 09:04:14 +0900

    test: StockPriceCollector 테스트 추가

--- stock-price-service/stock-price-domain/stock-price-application-service/src/test/java/com/gyeryongbrother/pickandtest/stockprice/domain/service/StockPriceCollectorImplTest.java
+++ stock-price-service/stock-price-domain/stock-price-application-service/src/test/java/com/gyeryongbrother/pickandtest/stockprice/domain/service/StockPriceCollectorImplTest.java
@@ -1,21 +1,82 @@
 package com.gyeryongbrother.pickandtest.stockprice.domain.service;
 
+import static com.gyeryongbrother.pickandtest.stockprice.domain.fixture.entity.StockPriceFixture.stockPricesByStockId;
+import static com.gyeryongbrother.pickandtest.stockprice.domain.fixture.valueobject.LocalDateFixture.januaryFirst;
+import static org.mockito.BDDMockito.any;
+import static org.mockito.BDDMockito.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.times;
+import static org.mockito.BDDMockito.verify;
+
+import com.gyeryongbrother.pickandtest.stockprice.domain.core.entity.StockPrice;
+import com.gyeryongbrother.pickandtest.stockprice.domain.core.valueobject.StockPriceDate;
+import com.gyeryongbrother.pickandtest.stockprice.domain.service.ports.input.StockPriceCollector;
+import com.gyeryongbrother.pickandtest.stockprice.domain.service.ports.output.MockStockPriceFetcher;
+import com.gyeryongbrother.pickandtest.stockprice.domain.service.ports.output.StockPriceQueryRepository;
+import com.gyeryongbrother.pickandtest.stockprice.domain.service.ports.output.StockPriceRepository;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("주가 정보를 가져와 저장한다")
 class StockPriceCollectorImplTest {
 
+    @Mock
+    private StockPriceRepository stockPriceRepository;
+
+    @Mock
+    private StockPriceQueryRepository stockPriceQueryRepository;
+
+    private StockPriceCollector stockPriceCollector;
+
+    private Map<Long, List<StockPrice>> stockPricesByStockId;
+
+    @BeforeEach
+    void setUp() {
+        stockPricesByStockId = stockPricesByStockId();
+        stockPriceCollector = new StockPriceCollectorImpl(
+                stockPriceRepository,
+                stockPriceQueryRepository,
+                new MockStockPriceFetcher(stockPricesByStockId)
+        );
+    }
+
     @Test
     @DisplayName("주가 정보가 없으면 모든 기간을 가져와 저장한다")
-    void collectStockPrices() {
+    void collectAllStockPrices() {
         // given
+        List<StockPrice> stockPrices = stockPricesByStockId.get(1L);
+        given(stockPriceQueryRepository.findLastDateOfStockPricesByStockId(anyLong()))
+                .willReturn(StockPriceDate.EMPTY);
 
         // when
+        stockPriceCollector.collectStockPrices(1L);
 
         // then
+        verify(stockPriceRepository, times(2)).save(any());
+        verify(stockPriceRepository).save(stockPrices.get(0));
+        verify(stockPriceRepository).save(stockPrices.get(1));
+    }
+
+    @Test
+    @DisplayName("주가 정보가 있으면 이후 모든 주가를 가져와 저장한다")
+    void collectStockPrices() {
+        // given
+        List<StockPrice> stockPrices = stockPricesByStockId.get(1L);
+        given(stockPriceQueryRepository.findLastDateOfStockPricesByStockId(anyLong()))
+                .willReturn(new StockPriceDate(januaryFirst()));
+
+        // when
+        stockPriceCollector.collectStockPrices(1L);
+
+        // then
+        verify(stockPriceRepository, times(1)).save(any());
+        verify(stockPriceRepository).save(stockPrices.get(1));
     }
 }
