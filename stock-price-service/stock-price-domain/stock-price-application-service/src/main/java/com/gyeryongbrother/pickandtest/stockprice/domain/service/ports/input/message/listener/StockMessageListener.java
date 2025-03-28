bf908772f0faaad06883cbcc36218c18c00fd8f5
odo27 <odo27@naver.com>
2025-03-28 10:23:01 +0900

    feat: StockMessageListener 구현

---
+++ stock-price-service/stock-price-domain/stock-price-application-service/src/main/java/com/gyeryongbrother/pickandtest/stockprice/domain/service/ports/input/message/listener/StockMessageListener.java
@@ -0,0 +1,8 @@
+package com.gyeryongbrother.pickandtest.stockprice.domain.service.ports.input.message.listener;
+
+import com.gyeryongbrother.pickandtest.stockprice.domain.core.entity.Stock;
+
+public interface StockMessageListener {
+
+    void stockCreated(Stock stock);
+}
