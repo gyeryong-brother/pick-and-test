48505f43426e9e42f8849a3ae7a533aed1ffe4bc
odo27 <odo27@naver.com>
2025-04-01 11:13:46 +0900

    feat: StockPriceScheduler 추가

--- stock-price-service/stock-price-domain/stock-price-application-service/src/main/java/com/gyeryongbrother/pickandtest/stockprice/domain/service/ports/output/StockPriceQueryRepository.java
+++ stock-price-service/stock-price-domain/stock-price-application-service/src/main/java/com/gyeryongbrother/pickandtest/stockprice/domain/service/ports/output/StockPriceQueryRepository.java
@@ -2,12 +2,13 @@ package com.gyeryongbrother.pickandtest.stockprice.domain.service.ports.output;
 
 import com.gyeryongbrother.pickandtest.stockprice.domain.core.entity.StockPrice;
 import com.gyeryongbrother.pickandtest.stockprice.domain.core.valueobject.StockPriceDate;
-import java.time.LocalDate;
 import java.util.List;
 
 public interface StockPriceQueryRepository {
 
     List<StockPrice> findAllByStockId(Long stockId);
+
+    List<Long> findAllStockIds();
 
     StockPriceDate findLastDateOfStockPricesByStockId(Long stockId);
 }
