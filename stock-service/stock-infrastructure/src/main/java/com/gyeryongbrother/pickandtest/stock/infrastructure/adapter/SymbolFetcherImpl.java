67ba52ff5c05e1c6737534ef0dd8022a2747eaef
odo27 <odo27@naver.com>
2025-04-19 21:32:40 +0900

    feat: tickers 리소스 추가

--- stock-service/stock-infrastructure/src/main/java/com/gyeryongbrother/pickandtest/stock/infrastructure/adapter/SymbolFetcherImpl.java
+++ stock-service/stock-infrastructure/src/main/java/com/gyeryongbrother/pickandtest/stock/infrastructure/adapter/SymbolFetcherImpl.java
@@ -1,23 +1,32 @@
 package com.gyeryongbrother.pickandtest.stock.infrastructure.adapter;
 
+import static com.gyeryongbrother.pickandtest.stock.infrastructure.exception.StockInfrastructureExceptionType.RESOURCE_READ_FAILED;
+
 import com.gyeryongbrother.pickandtest.stock.domain.core.valueobject.StockExchange;
 import com.gyeryongbrother.pickandtest.stock.domain.service.ports.output.SymbolFetcher;
-import com.gyeryongbrother.pickandtest.stock.infrastructure.api.gyeryongbrother.DataServiceClient;
-import com.gyeryongbrother.pickandtest.stock.infrastructure.api.gyeryongbrother.dto.TickersResponse;
+import com.gyeryongbrother.pickandtest.stock.infrastructure.exception.StockInfrastructureException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class SymbolFetcherImpl implements SymbolFetcher {
 
-    private final DataServiceClient dataServiceClient;
-
     @Override
     public List<String> fetchSymbols(StockExchange stockExchange) {
-        String exchange = stockExchange.name().toLowerCase();
-        TickersResponse tickersResponse = dataServiceClient.fetchTickers(exchange);
-        return tickersResponse.tickers();
+        ClassPathResource resource = new ClassPathResource(stockExchange.name().toLowerCase() + ".txt");
+        try (InputStream inputStream = resource.getInputStream();
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return reader.lines().toList();
+        } catch (IOException e) {
+            throw new StockInfrastructureException(RESOURCE_READ_FAILED);
+        }
     }
 }
