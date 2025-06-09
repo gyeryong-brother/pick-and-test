12435035561cc76216fb968b113d7e4031ea9bb4
odo27 <odo27@naver.com>
2025-06-09 15:58:29 +0900

    feat: oauth 기능 구현

--- authentication-service/authentication-infrastructure/src/main/java/com/gyeryongbrother/pickandtest/authentication/infrastructure/exception/AuthenticationInfrastructureExceptionType.java
+++ authentication-service/authentication-infrastructure/src/main/java/com/gyeryongbrother/pickandtest/authentication/infrastructure/exception/AuthenticationInfrastructureExceptionType.java
@@ -9,6 +9,7 @@ public enum AuthenticationInfrastructureExceptionType implements BaseExceptionTy
 
     USERNAME_PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST, "사용자 정보가 일치하지 않습니다."),
     INVALID_TOKEN_ERROR(HttpStatus.UNAUTHORIZED, "Invalid Token Error"),
+    OAUTH_SERVER_NOT_SUPPORTED(HttpStatus.BAD_REQUEST, "지원하지 않는 서버 타입입니다."),
     ;
 
     private final HttpStatus httpStatus;
---
+++ authentication-service/authentication-infrastructure/src/main/java/com/gyeryongbrother/pickandtest/authentication/infrastructure/oauth/kakao/KakaoOauthConfig.java
@@ -0,0 +1,42 @@
+package com.gyeryongbrother.pickandtest.authentication.infrastructure.oauth.kakao;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KakaoOauthConfig {
+
+    private final String clientId;
+    private final String redirectUri;
+    private final String clientSecret;
+    private final List<String> scope;
+
+    public KakaoOauthConfig(
+            @Value("${oauth.kakao.client_id}") String clientId,
+            @Value("${oauth.kakao.redirect_uri}") String redirectUri,
+            @Value("${oauth.kakao.client_secret}") String clientSecret,
+            @Value("${oauth.kakao.scope}") List<String> scope
+    ) {
+        this.clientId = clientId;
+        this.redirectUri = redirectUri;
+        this.clientSecret = clientSecret;
+        this.scope = scope;
+    }
+
+    public String clientId() {
+        return clientId;
+    }
+
+    public String redirectUri() {
+        return redirectUri;
+    }
+
+    public String clientSecret() {
+        return clientSecret;
+    }
+
+    public List<String> scope() {
+        return scope;
+    }
+}
