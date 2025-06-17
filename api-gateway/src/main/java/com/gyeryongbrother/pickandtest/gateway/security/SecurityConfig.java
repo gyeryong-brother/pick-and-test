91a78aac7c6e9350452646fe1fe1ba94f6335d49
odo27 <odo27@naver.com>
2025-06-17 16:00:02 +0900

    feat: Spring Cloud 추가

---
+++ api-gateway/src/main/java/com/gyeryongbrother/pickandtest/gateway/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.gyeryongbrother.pickandtest.gateway.security;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/authentication-service/auth/logout").authenticated()
+                .requestMatchers("/authentication-service/auth/**").permitAll()
+                .anyRequest().authenticated()
+        ).oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
+        return http.build();
+    }
+}
