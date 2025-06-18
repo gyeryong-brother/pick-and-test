1c2854249c81761a422c38875e7545ad2507cac4
odo27 <odo27@naver.com>
2025-06-18 11:16:52 +0900

    feat: oauth-client 추가

---
+++ authentication-service/authentication-infrastructure/src/main/java/com/gyeryongbrother/pickandtest/authentication/infrastructure/jwt/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,18 @@
+package com.gyeryongbrother.pickandtest.authentication.infrastructure.jwt;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException exception
+    ) {
+
+    }
+}
