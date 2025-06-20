31835b3d8949b4e5ca9930c224208ee5a8fc95aa
odo27 <odo27@naver.com>
2025-06-20 14:51:08 +0900

    chore: eureka service registry 추가

--- member-service/member-application/src/main/java/com/gyeryongbrother/pickandtest/member/application/rest/MemberController.java
+++ member-service/member-application/src/main/java/com/gyeryongbrother/pickandtest/member/application/rest/MemberController.java
@@ -8,6 +8,7 @@ import com.gyeryongbrother.pickandtest.member.domain.service.dto.RegisterMemberR
 import com.gyeryongbrother.pickandtest.member.domain.service.ports.input.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,5 +27,10 @@ public class MemberController {
         RegisterMemberResponse registerMemberResponse = memberService.register(command);
         return ResponseEntity.status(CREATED)
                 .body(registerMemberResponse);
+    }
+
+    @GetMapping
+    ResponseEntity<String> test() {
+        return ResponseEntity.ok("test!");
     }
 }
