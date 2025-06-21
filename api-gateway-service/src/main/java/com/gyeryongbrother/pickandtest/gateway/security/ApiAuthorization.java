eca20188332ead0828773160d9b481f6c28a38d6
odo27 <odo27@naver.com>
2025-06-22 03:13:33 +0900

    feat: jwt header 변환 로직 구현

--- api-gateway-service/src/main/java/com/gyeryongbrother/pickandtest/gateway/security/ApiAuthorization.java
+++ api-gateway-service/src/main/java/com/gyeryongbrother/pickandtest/gateway/security/ApiAuthorization.java
@@ -1,20 +1,10 @@
 package com.gyeryongbrother.pickandtest.gateway.security;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import org.springframework.security.config.web.server.ServerHttpSecurity.AuthorizeExchangeSpec;
-import org.springframework.security.config.web.server.ServerHttpSecurity.OAuth2ResourceServerSpec;
 
 public class ApiAuthorization {
 
-    private static final String AUTH_API = "/authentication-service/**";
-
     public static void rule(AuthorizeExchangeSpec exchange) {
-        exchange.pathMatchers(AUTH_API).permitAll()
-                .anyExchange().authenticated();
-    }
-
-    public static void method(OAuth2ResourceServerSpec oAuth) {
-        oAuth.jwt(withDefaults());
+        exchange.anyExchange().permitAll();
     }
 }
