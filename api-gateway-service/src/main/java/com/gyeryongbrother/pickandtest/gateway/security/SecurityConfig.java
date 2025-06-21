e588cbbbcf6cebe2eb7d1ad39415a1dbe120311d
odo27 <odo27@naver.com>
2025-06-22 03:13:33 +0900

    feat: jwt header 변환 로직 구현

--- api-gateway-service/src/main/java/com/gyeryongbrother/pickandtest/gateway/security/SecurityConfig.java
+++ api-gateway-service/src/main/java/com/gyeryongbrother/pickandtest/gateway/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -15,7 +16,14 @@ public class SecurityConfig {
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http.csrf(CsrfSpec::disable)
                 .authorizeExchange(ApiAuthorization::rule)
-                .oauth2ResourceServer(ApiAuthorization::method)
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(
+                                        new ReactiveJwtAuthenticationConverterAdapter(
+                                                new CustomJwtAuthenticationConverter()
+                                        ))
+                        )
+                )
                 .build();
     }
 }
