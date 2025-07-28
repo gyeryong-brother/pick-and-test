3abee1f0e5fbfbacf05a6804eff879223e9c3dff
jusikjoa <brian20037463@gmail.com>
2025-07-28 16:56:49 +0900

    feat: 포트폴리오 저장 및 삭제 서비스 추가

--- portfolio-service/portfolio-domain/portfolio-application-service/src/main/java/com/gyeryongbrother/pickandtest/portfolio/domain/service/PortfolioServiceImpl.java
+++ portfolio-service/portfolio-domain/portfolio-application-service/src/main/java/com/gyeryongbrother/pickandtest/portfolio/domain/service/PortfolioServiceImpl.java
@@ -1,12 +1,16 @@
 package com.gyeryongbrother.pickandtest.portfolio.domain.service;
 
 import com.gyeryongbrother.pickandtest.portfolio.domain.core.entity.Portfolio;
+import com.gyeryongbrother.pickandtest.portfolio.domain.service.dto.DeletePortfolioCommand;
+import com.gyeryongbrother.pickandtest.portfolio.domain.service.dto.PortfolioResponse;
+import com.gyeryongbrother.pickandtest.portfolio.domain.service.dto.PortfoliosResponse;
 import com.gyeryongbrother.pickandtest.portfolio.domain.service.dto.UpdatePortfolioCommand;
 import com.gyeryongbrother.pickandtest.portfolio.domain.service.dto.UpdatePortfolioResponse;
 import com.gyeryongbrother.pickandtest.portfolio.domain.service.ports.input.PortfolioService;
 import com.gyeryongbrother.pickandtest.portfolio.domain.service.ports.output.PortfolioQueryRepository;
 import com.gyeryongbrother.pickandtest.portfolio.domain.service.ports.output.PortfolioRepository;
 import com.gyeryongbrother.pickandtest.portfolio.domain.service.ports.output.PortfolioStockRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,5 +31,22 @@ public class PortfolioServiceImpl implements PortfolioService {
         portfolioStockRepository.saveAll(portfolio.getPortfolioStocks());
         Portfolio updated = portfolioQueryRepository.findById(portfolio.getId());
         return UpdatePortfolioResponse.from(updated);
+    }
+
+    @Override
+    public PortfoliosResponse deletePortfolioId(DeletePortfolioCommand deletePortfolioCommand) {
+        portfolioRepository.delete(deletePortfolioCommand.portfolioId());
+        List<Portfolio> remained=portfolioQueryRepository.findAllByMemberId(deletePortfolioCommand.memberId());
+        List<PortfolioResponse> portfolioResponses=remained.stream()
+                .map(PortfolioResponse::from)
+                .toList();
+        return new PortfoliosResponse(portfolioResponses);
+    }
+
+    @Override
+    public UpdatePortfolioResponse savePortfolio(UpdatePortfolioCommand updatePortfolioCommand) {
+        Portfolio portfolio=updatePortfolioCommand.toDomain(updatePortfolioCommand.portfolioId());
+        Portfolio saved=portfolioRepository.save(portfolio);
+        return UpdatePortfolioResponse.from(saved);
     }
 }
--- portfolio-service/portfolio-domain/portfolio-application-service/src/main/java/com/gyeryongbrother/pickandtest/portfolio/domain/service/ports/input/PortfolioService.java
+++ portfolio-service/portfolio-domain/portfolio-application-service/src/main/java/com/gyeryongbrother/pickandtest/portfolio/domain/service/ports/input/PortfolioService.java
@@ -1,9 +1,15 @@
 package com.gyeryongbrother.pickandtest.portfolio.domain.service.ports.input;
 
+import com.gyeryongbrother.pickandtest.portfolio.domain.service.dto.DeletePortfolioCommand;
+import com.gyeryongbrother.pickandtest.portfolio.domain.service.dto.PortfoliosResponse;
 import com.gyeryongbrother.pickandtest.portfolio.domain.service.dto.UpdatePortfolioCommand;
 import com.gyeryongbrother.pickandtest.portfolio.domain.service.dto.UpdatePortfolioResponse;
 
 public interface PortfolioService {
 
     UpdatePortfolioResponse updatePortfolio(UpdatePortfolioCommand updatePortfolioCommand);
+
+    PortfoliosResponse deletePortfolioId(DeletePortfolioCommand deletePortfolioCommand);
+
+    UpdatePortfolioResponse savePortfolio(UpdatePortfolioCommand updatePortfolioCommand);
 }
