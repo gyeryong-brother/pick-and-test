324e7c78badc373232802bb0654fb6649ff5d3b4
jusikjoa <brian20037463@gmail.com>
2025-09-10 11:21:27 +0900

    feat: redis 적용

--- stock-price-service/stock-price-domain/stock-price-application-service/src/test/java/com/gyeryongbrother/pickandtest/stockprice/domain/service/StockPriceQueryServiceImplTest.java
+++ stock-price-service/stock-price-domain/stock-price-application-service/src/test/java/com/gyeryongbrother/pickandtest/stockprice/domain/service/StockPriceQueryServiceImplTest.java
@@ -17,6 +17,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("주가 조회 서비스를 구현한다")
