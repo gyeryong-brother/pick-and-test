b69d687f86a0a8d80d9f0cec43e906b18792fb01
jusikjoa <brian20037463@gmail.com>
2025-09-10 11:43:07 +0900

    fix: redis 의존성 제거

--- stock-price-service/stock-price-domain/stock-price-application-service/src/test/java/com/gyeryongbrother/pickandtest/stockprice/domain/service/StockPriceQueryServiceImplTest.java
+++ stock-price-service/stock-price-domain/stock-price-application-service/src/test/java/com/gyeryongbrother/pickandtest/stockprice/domain/service/StockPriceQueryServiceImplTest.java
@@ -17,8 +17,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
+
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("주가 조회 서비스를 구현한다")
